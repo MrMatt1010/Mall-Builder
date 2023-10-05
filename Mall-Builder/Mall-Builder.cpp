192c9aa2715c8ed1cd97482d34f2641311c79332
Matt T <mrsymtec@gmail.com>
2023-10-06 10:35:53 +1300

    Added child classes and overrides for the parent class.

--- Mall-Builder/Mall-Builder.cpp
+++ Mall-Builder/Mall-Builder.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 //Parent Class
-class shapes {
+class Shapes {
 protected:
 	double base;
 	double height;
@@ -14,7 +14,7 @@ protected:
 
 public:
 // Constructor for the parent class
-	shapes(double b, double h) : base(b), height(h), result(0.0) {}
+	Shapes(double b, double h) : base(b), height(h), result(0.0) {}
 
 // Function to get input data
 	void getData() {
@@ -26,7 +26,7 @@ public:
 //Function to calculate the area (to be overridden by the child classes)
 	virtual void calculateArea() {}
 
-//Function to calculate the preimeter (to be overridden by the child classes)
+//Function to calculate the perimeter (to be overridden by the child classes)
 	virtual void calculatePerimeter() {}
 
 //Function to display the results
@@ -35,7 +35,39 @@ public:
 	}
 };
 
+//Child Class Square
+class Square : public Shapes {
+public:
+//Constructor the the Square class
+	Square(double side); Shapes(side, side) {}
 
+//Function to calculate the area for the square
+	void calculateArea() override {
+		result = base * base;
+	}
+
+//Function to calculate the perimeter of the square
+	void calculatePerimeter() {
+		result = 4 * base;
+	}
+};
+
+//Child Class Rectangle
+class Rectangle : public Shapes {
+public:
+	//Constructor for the rectangle class
+	Rectangle(double l, double w) : Shapes(l, w) {}
+
+//Function to calculate the area for a Rectangle
+	void calculateArea() override {
+		result =  base + height;
+	}
+
+//Function to calculate the perimeter for the Rectangle
+	void calculatePerimeter() override {
+		result = 2 * base + height;
+	}
+};
 
 int main()
 {
